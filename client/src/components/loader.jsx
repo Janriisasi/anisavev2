a9685cf6e036265a65eeff1270ded5c9611713e0
Sasi <johnreysasi55@gmail.com>
2025-08-02 18:59:35 +0800

    added a loading screen

---
+++ client/src/components/loader.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import aniLogo from '../assets/ani_logo.svg';
+import anisave from '../assets/anisave_logo.png';
+
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 bg-white z-50 flex flex-col items-center justify-center">
+      <div className="relative">
+        <div className="w-28 h-28 border-8 text-green-800 animate-spin border-gray-300 border-t-green-800 rounded-full" />
+        <img 
+          src={aniLogo}
+          alt="AniSave"
+          className="w-16 h-16 object-contain absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
