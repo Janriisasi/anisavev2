b175db5d8cf5f91ce2548c77e02e22e94a83839a
Sasi <johnreysasi55@gmail.com>
2025-09-16 16:28:32 +0800

    fixed the colors of the input boxes

--- client/src/pages/signupPage.jsx
+++ client/src/pages/signupPage.jsx
@@ -148,7 +148,7 @@ function SignUp() {
                 </label>
                 <input
                   id="full_name"
-                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-green-700"
                   name="full_name"
                   value={form.full_name}
                   onChange={handleChange}
@@ -162,7 +162,7 @@ function SignUp() {
                 </label>
                 <input
                   id="username"
-                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-green-700"
                   name="username"
                   value={form.username}
                   onChange={handleChange}
@@ -178,7 +178,7 @@ function SignUp() {
                 </label>
                 <input
                   id="email"
-                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                  className="w-full px-4 py-3 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-green-700"
                   type="email"
                   name="email"
                   value={form.email}
@@ -194,7 +194,7 @@ function SignUp() {
                 <div className="relative">
                   <input
                     id="password"
-                    className="w-full px-4 py-3 pr-12 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                    className="w-full px-4 py-3 pr-12 border-2 border-black rounded-xl focus:outline-none focus:ring-2 focus:ring-green-700 focus:border-green-700"
                     type={showPassword ? "text" : "password"}
                     name="password"
                     value={form.password}
@@ -206,7 +206,7 @@ function SignUp() {
                   <button
                     type="button"
                     onClick={togglePasswordVisibility}
-                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+                    className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-green-700 focus:outline-none"
                     tabIndex={-1}
                   >
                     {showPassword ? (
@@ -219,7 +219,7 @@ function SignUp() {
                 
                 {/* password requirements */}
                 {showPasswordRequirements && (
-                  <div className="mt-3 p-3 bg-gray-50 border border-gray-200 rounded-lg">
+                  <div className="mt-3 p-3 bg-gray-50 border border-gray-500 rounded-lg">
                     {(!hasMinLength || !hasUpperCase || !hasLowerCase || !hasNumber || !hasSpecialChar) && (
                       <p className="text-sm font-medium text-gray-700 mb-2">Password Requirements:</p>
                     )}
